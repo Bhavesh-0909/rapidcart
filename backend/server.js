a59426dc83e5d94231c66d9c4165f29ca9e2d4ee
Bhavesh-0909 <bhaveshchoudhary0909@gmail.com>
2025-03-16 13:28:08 +0530

    add Supabase integration for user data retrieval

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const app = require('./app');
-
+const supabase = require('./database/supabase')
 
 // Basic route
 app.get('/', (req, res) => {
     res.send('RapidCart API is running');
+});
+
+app.get('/users', async (req, res) => {
+    try {
+        const { data:users , error } = await supabase.from('users').select('*');
+        if (error) throw error;
+        return res.send(users);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 });
 
 // Start server
